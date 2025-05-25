38906d9639116187b7a1c29374cdb400dd1a74a7
kanishq.maheshwari2003@gmail.com <121596849+kanishq-17@users.noreply.github.com>
2025-05-25 06:44:12 +0530

    Validate User Schema and API Validation

--- src/index.js
+++ src/index.js
@@ -6,55 +6,133 @@ const PORT = 3000;
 
 const app = express();
 
+// middleware
 app.use(express.json());
 
-app.get("/getUser", async (req, res) => {
-  const username = req.body.username;
-
+// Post user to database
+app.post("/signup", async (req, res) => {
   try {
-    const user = await User.find({ username: username });
+    const user = new User(req.body);
 
-    if (user.length === 0) {
-      res.status(400).send({ message: "Something went wrong" });
+    const savedUser = await user.save();
+
+    if (user) {
+      res.status(201).send({
+        message: "User Created",
+        User: {
+          id: savedUser._id,
+          name: savedUser.firstName,
+          email: savedUser.email,
+        },
+      });
     } else {
-      console.log("User Found");
-      res.send(user);
+      res.status(400).send("Something Went Wrong\nCannot Created User");
+    }
+  } catch (err) {
+    res.status(500).send({ Error: err.message });
+  }
+});
+
+// Get user from database
+app.get("/getUsers", async (req, res) => {
+  const { username, email } = req.query;
+  try {
+    const allUser = await User.find({ username, email });
+
+    if (allUser.length !== 0) {
+      res.status(302).send(allUser);
+    } else {
+      res.status(404).send("Users Data Not Found");
     }
   } catch (err) {
     res.status(500).send({
-      error: err.message,
+      Error: err.message,
     });
   }
 });
 
-app.post("/signup", async (req, res) => {
+// Delete user from databse
+app.delete("/deleteUser", async (req, res) => {
   try {
-    // creating new instance of user
-    const user = new User(req.body);
+    const firstName = req.body.firstName;
+    const lastName = req.body.lastName;
+    const email = req.body.email;
 
-    // saving user to database
-    const savedUser = await user.save();
-
-    // user created successfully message sent to console
-    console.log("User Created Successfully");
-
-    // creating user and sending response
-    res.status(201).send({
-      message: "User Created",
-      User: {
-        id: savedUser._id,
-        name: savedUser.firstName,
-        email: savedUser.email,
-      },
+    const deletedUser = await User.findOneAndDelete({
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
     });
-  } catch (errr) {
+
+    if (deletedUser) {
+      res.status(200).send({
+        message: "User Deleted Successfully",
+        User: {
+          deletedUser,
+        },
+      });
+    } else {
+      res.status(404).send("Users Data Not Found");
+    }
+  } catch (err) {
     res.status(500).send({
-      error: errr.message,
+      Error: err.message,
+    });
+  }
+});
+
+// Update user in database
+app.patch("/updateUser", async (req, res) => {
+  try {
+    // const userId = req.body.userId;
+    const { username, password } = req.query;
+    const data = req.body;
+
+    const ALLOWED_UPDATES = ["username", "password", "photo", "gender"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k)
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update not allowed");
+    }
+
+    if (data?.username) {
+      const existingUser = await User.findOne({ username: data.username });
+      if (existingUser && existingUser.username !== username) {
+        return res.status(400).send({ Error: "Username already exists!" });
+      }
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+      { username, password },
+      data,
+      {
+        returnDocument: "after",
+      }
+    );
+
+    if (updatedUser) {
+      res.status(200).send({
+        message: "User Updated Successfully",
+        User: {
+          updatedUser,
+        },
+      });
+    } else {
+      res.status(404).send("Users Data Not Found");
+    }
+  } catch (err) {
+    res.status(500).send({
+      Error: err.message,
     });
   }
 });
 
 ConnectDB().then(() => {
-  console.log("DB Connected Successfully !");
-  app.listen(PORT, () => console.log("Server is running on Port:", PORT));
+  console.log("DB Connected Successfully");
+  app.listen(PORT, () => {
+    console.log("Server is running on Port:", PORT);
+  });
 });
--- src/models/User.js
+++ src/models/User.js
@@ -1,25 +1,79 @@
 const mongoose = require("mongoose");
-
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const validate = require("validate");
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    lastName: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 100,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      minlength: 2,
+      validate(value) {
+        if (!validator.isEmail(value)) {
+          throw new Error("Email id not valid", value);
+        }
+      },
+    },
+    password: {
+      type: String,
+      required: true,
+      lowercase: true,
+      trim: true,
+      minlength: 10,
+      maxlength: 50,
+      validate(value) {
+        if (!validator.isStrongPassword(value)) {
+          throw new Error("Enter a Strong password", value);
+        }
+      },
+    },
+    photo: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      default:
+        "https://imgs.search.brave.com/AlbD1PakFJCd1uiCZOL2E0g_yv009u1CVuiRssRwXLI/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9pbWdz/LnNlYXJjaC5icmF2/ZS5jb20vMGc1SzdR/cjVwdGdfdTRDc1pG/QVNrRG9HTTE2NS1j/eVlTeHhfdW5QUjlw/by9yczpmaXQ6NTAw/OjA6MDowL2c6Y2Uv/YUhSMGNITTZMeTlw/YldjdS9abkpsWlhC/cGF5NWpiMjB2L2NI/SmxiV2wxYlMxMlpX/TjAvYjNJdmMybHNk/bVZ5TFcxbC9iV0ps/Y25Ob2FYQXRhV052/L2JpMWtaV1poZFd4/MExXRjIvWVhSaGNp/MXdjbTltYVd4bC9M/V2xqYjI0dGJXVnRZ/bVZ5L2MyaHBjQzFw/WTI5dUxYTnYvWTJs/aGJDMXRaV1JwWVMx/MS9jMlZ5TFdsdFlX/ZGxMWFpsL1kzUnZj/aTFwYkd4MWMzUnkv/WVhScGIyNWZOVFl4/TVRVNC9MVFF4T1RV/dWFuQm5QM05sL2JY/UTlZV2x6WDJoNVlu/SnAvWkNaM1BUYzBN/QQ.jpeg",
+    },
+    about: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      default: "This is default description of about user",
+    },
+    age: {
+      type: Number,
+      trim: true,
+      min: 15,
+    },
+    gender: { type: String, lowercase: true, trim: true },
   },
-  lastName: {
-    type: String,
-  },
-  username: {
-    type: String,
-  },
-  email: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
